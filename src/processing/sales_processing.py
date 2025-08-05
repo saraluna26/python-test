7f87cac29ec0407c03c77e02aba9937d23f8ebd6
saraluna26 <79517333+saraluna26@users.noreply.github.com>
2025-08-05 12:02:06 +0100

    Stub

---
+++ src/processing/sales_processing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def null_values(df: Dataframe) -> Dataframe:
+    pass
+
+def group_by_customer(df:Dataframe, customer_id: int) -> Dataframe: 
+    pass
+
+def group_by_category(df:Dataframe, customer_id: int) -> Dataframe: 
+    pass
+
+def normalize_string_cols(df:Dataframe) -> DataFrame: 
+    pass
+
+def cast_dates(df:DataFrame) -> DataFrame:
+    pass
+
+def get_category_income(df:DataFrame) -> DataFrame:
+    pass
+
---
+++ src/processing/sales_utils.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+
+def load_data(path: string) -> DataFrame:
+    pass
+
+def write_data(path: string) -> DataFrame:
+    pass
